18e3848729e40a31b5c9fa7ecc345c98e5ba9a9b
Jawad <Jawad.Nazari@outlook.com>
2023-05-24 00:22:23 +0500

    Updating the navlink component with routes

--- src/components/navLink.js
+++ src/components/navLink.js
@@ -1,11 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
+import Missions from './missions';
+import MyProfile from './myProfile';
 import Rockets from './rockets';
 
 const NavLink = () => (
   <main className="app-container">
     <Routes>
-      <Route path="/" element={<Rockets />} exact />
-      <Route path="/rockets" element={<Rockets />} exact />
+      <Route path="/rockets" element={<Rockets />} />
+      <Route path="/missions" element={<Missions />} />
+      <Route path="/myProfile" element={<MyProfile />} />
     </Routes>
   </main>
 );
