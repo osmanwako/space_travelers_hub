feb532241203de1646ed661fc1f59c85377570d1
OsmanFP <osmanwako411@gmail.com>
2023-05-23 15:23:03 +0300

    Add empty navigation components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import logo from './assets/planet.png';
 import './App.css';
+import NavLink from './components/navLink';
+import HeaderLink from './components/headerLink';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderLink />
+      <NavLink />
     </div>
   );
 }
