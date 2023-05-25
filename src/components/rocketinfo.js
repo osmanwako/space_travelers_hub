4d51348397302cf834bee32a9fedc96fdb8de19d
OsmanFP <osmanwako411@gmail.com>
2023-05-25 18:59:49 +0300

    Add reserve button badge conditionally

--- src/components/rocketinfo.js
+++ src/components/rocketinfo.js
@@ -2,15 +2,18 @@ function RocketInformation(data) {
   const { rocket } = data;
   return (
     <>
-      <div className="grid-item image-item">
+      <div className="rocket-item rocket-gallery">
         <img src={rocket.flickrimages[0]} alt="space travelers" className="rocket-img" />
       </div>
-      <div className="grid-item desc-item">
+      <div className="rocket-item rocket-desc">
         <h3>
           {rocket.name}
         </h3>
         <p>
           {rocket.description}
+          <button type="button" className={rocket.reserved ? 'rbtn-cancel' : 'rbtn-reserve'}>
+            {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
+          </button>
         </p>
       </div>
     </>
