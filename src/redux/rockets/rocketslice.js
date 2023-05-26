6e2020043022883ee69da5ca6ca7defd92ee084e
Osman Wako <osmanwako411@gmail.com>
2023-05-26 15:19:28 +0300

    Update git conflict

--- src/redux/rockets/rocketslice.js
+++ src/redux/rockets/rocketslice.js
@@ -1,29 +1,73 @@
-import { useDispatch } from 'react-redux';
-import { reservation } from '../redux/rockets/rocketslice';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-function RocketInformation(data) {
-  const dispatch = useDispatch();
+const url = 'https://api.spacexdata.com/v4/rockets';
 
-  const { rocket } = data;
-  return (
-    <>
-      <div className="rocket-item rocket-gallery">
-        <img src={rocket.flickrimages[0]} alt="space travelers" className="rocket-img" />
-      </div>
-      <div className="rocket-item rocket-desc">
-        <h3>
-          {rocket.name}
-        </h3>
-        <p className="p-desc">
-          {rocket.reserved ? <span className="span-badge">Reserved</span> : ''}
-          {rocket.description}
-          <button type="button" className={rocket.reserved ? 'rbtn-cancel' : 'rbtn-reserve'} onClick={() => dispatch(reservation(rocket.id))}>
-            {rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
-          </button>
-        </p>
-      </div>
-    </>
-  );
-}
+const initialState = {
+  rockets: [],
+  status: 'idle',
+  load: Date.now(),
+  message: 'Everything is good',
+  total: 0,
+};
+const generaterockets = (lists) => {
+  const rockets = lists.map((list) => {
+    const {
+      id, name, description,
+    } = list;
+    const flickrimages = list.flickr_images;
+    const reserved = false;
+    return {
+      id, name, description, flickrimages, reserved,
+    };
+  });
+  return rockets;
+};
+export const fetchRockets = createAsyncThunk('rocketstores/fetchRockets', async () => {
+  const response = await axios.get(url);
+  const { data } = response;
+  return generaterockets(data);
+});
 
-export default RocketInformation;
+export const Rocketslice = createSlice({
+  name: 'rocketstore',
+  initialState,
+  reducers: {
+    reservation: (state, action) => {
+      const rocket = state.rockets.find((item) => item.id === action.payload);
+      if (rocket) {
+        rocket.reserved = !rocket.reserved;
+      }
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchRockets.pending, (state) => {
+      const newstate = {
+        ...state,
+        status: 'pending',
+        rockets: [],
+      };
+      return { ...newstate };
+    }).addCase(fetchRockets.fulfilled, (state, action) => {
+      const newstate = {
+        ...state,
+        status: 'fulfilled',
+        rockets: action.payload,
+      };
+      return { ...newstate };
+    }).addCase(fetchRockets.rejected, (state, action) => {
+      const newstate = {
+        ...state,
+        status: 'rejected',
+        message: action.payload,
+        rockets: [],
+      };
+      return { ...newstate };
+    });
+  },
+});
+
+export const { reservation } = Rocketslice.actions;
+
+export default Rocketslice.reducer;
+
